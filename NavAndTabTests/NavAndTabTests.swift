95f0335fef96008555849fdd650b53d17836f88b
Icaro Lavrador <icarolavrador@gmail.com>
2015-09-03 19:58:24 +1200

    Initial Commit

---
+++ NavAndTabTests/NavAndTabTests.swift
@@ -0,0 +1,36 @@
+//
+//  NavAndTabTests.swift
+//  NavAndTabTests
+//
+//  Created by Icaro Barreira Lavrador on 3/09/15.
+//  Copyright (c) 2015 Icaro Barreira Lavrador. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NavAndTabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
