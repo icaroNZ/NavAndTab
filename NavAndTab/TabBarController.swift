e790511fafd7c7dae38cffdd41adb222c8ee711c
Icaro Lavrador <icarolavrador@gmail.com>
2015-09-03 20:05:29 +1200

    Initial commit

--- NavAndTab/TabBarController.swift
+++ NavAndTab/TabBarController.swift
@@ -1,31 +1,39 @@
 //
-//  ViewController.swift
+//  TabBarController.swift
 //  udemyApp
 //
-//  Created by Icaro Barreira Lavrador on 1/09/15.
+//  Created by Icaro Barreira Lavrador on 3/09/15.
 //  Copyright (c) 2015 Icaro Barreira Lavrador. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        performSegueWithIdentifier("segue", sender: nil)
         self.navigationItem.leftBarButtonItem = nil
         self.navigationItem.hidesBackButton = true
         self.navigationItem.backBarButtonItem   = nil
         self.navigationItem.setHidesBackButton(true, animated: false)
         self.navigationItem.setLeftBarButtonItem(nil, animated: true)
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
